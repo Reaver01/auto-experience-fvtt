6a2cf4fea4a9798616d821cc6823a6287eecc90c
Reaver01 <jawmac@gmail.com>
2019-10-15 16:49:10 -0600

    Version 0.2

--- auto-experience/auto-experience.js
+++ auto-experience/auto-experience.js
@@ -1,10 +1,12 @@
 // @author Reaver#4634
 
-Hooks.on('deleteCombat', deletedCombat => {
+var addExperience;
+
+Hooks.on('preDeleteCombat', deletedCombat => {
     defeatedEnemies = deletedCombat.turns.filter(object => (!object.actor.isPC && object.defeated && object.token.disposition === -1));
     players = deletedCombat.turns.filter(object => (object.actor.isPC && !object.defeated));
     experience = 0;
-    if (defeatedEnemies.length > 0) {
+    if (defeatedEnemies.length > 0 && addExperience) {
         defeatedEnemies.forEach(enemy => {
             experience += enemy.actor.data.data.details.xp.value;
         });
@@ -13,15 +15,58 @@ Hooks.on('deleteCombat', deletedCombat => {
             experienceMessage = '<b>Experience Awarded!</b><p><b>' + dividedExperience + '</b> added to:</br>';
             players.forEach(player => {
                 index = game.actors.index(player.actor.data._id);
-                game.actors.entities[index].update({'data.details.xp.value': player.actor.data.data.details.xp.value + dividedExperience});
+                game.actors.entities[index].update({
+                    'data.details.xp.value': player.actor.data.data.details.xp.value + dividedExperience
+                });
                 experienceMessage += player.actor.data.name + '</br>';
             });
             experienceMessage += '</p>';
             ChatMessage.create({
                 user: game.user._id,
-                speaker: { actor: this.actor },
+                speaker: {
+                    actor: this.actor
+                },
                 content: experienceMessage
             });
         }
     }
 });
+
+class NewCombat {
+    async endCombat() {
+        return new Promise((resolve, reject) => {
+            if (!game.user.isGM) {
+                reject('You cannot end an active combat');
+            }
+            new Dialog({
+                title: `End Combat?`,
+                content: '<p>End this combat encounter and empty the turn tracker?</p>',
+                buttons: {
+                    yes: {
+                        icon: '<i class="fas fa-check"></i>',
+                        label: 'End Combat',
+                        callback: () => {
+                            addExperience = false;
+                            this.delete().then(resolve);
+                        }
+                    },
+                    xp: {
+                        icon: '<i class="fas fa-check"></i>',
+                        label: 'End with XP',
+                        callback: () => {
+                            addExperience = true;
+                            this.delete().then(resolve);
+                        }
+                    },
+                    no: {
+                        icon: '<i class="fas fa-times"></i>',
+                        label: 'Cancel'
+                    }
+                },
+                default: 'yes'
+            }).render(true);
+        });
+    }
+}
+
+Combat.prototype.endCombat = NewCombat.prototype.endCombat;
